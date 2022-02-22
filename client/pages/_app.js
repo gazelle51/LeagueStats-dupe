9a3c215d1b68c206a1a76a4d91f032c8e875961a
Sarah Gazelle <sarahgaz@au1.ibm.com>
2022-02-22 18:14:41 +1100

    Add tailwind css and navbar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>LeagueStats</title>
+        <meta
+          name='description'
+          content='LeagueStats.gg is an Open source website for League of Legends Summoners statistics.'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
